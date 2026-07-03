cf9cff7dbd09d33e1bd70f60fa56b7e16dc14fd7
nir <nirr0313@gmail.com>
2026-07-04 01:07:08 +0530

    python day2 class programs

---
+++ py_day2/1.py
@@ -0,0 +1,67 @@
+# 1. Write a python program to print "Hello World" 
+print("hello world")
+
+# 2. Write a python program to get user input using input( ) function 
+# 3. WAP to take only numerical input 
+
+#-> ;lets combine 2-3
+
+# two_three = int(input("enter only numerical data : "))
+# print(two_three)
+
+
+# 4. Convert the number to floating point number 
+
+#print(float(two_three))
+
+# 5. WAP to Add, Subtract, Multiply and Divide 2 numbers 
+
+one = 10
+two = 20
+
+print("Add : ", one+two)
+print("mul => ", one*two)
+print("sub => ", one-two)
+print("Div => ", one/two)
+
+
+
+
+# 6. Print the quotient and remainder separately for division operation 
+
+print("quotient => ", one//two)
+print("remainder => ", one%two)
+
+
+
+# 7. Write a program to find meter to kilometer. 
+
+#idk 7 
+
+# 8. Write a program to find area of a rectangle. (Area=l*b). Take input 
+# parameters from user 
+
+
+# 9. Write a program to find volume of cube. (Area=l*b*h). Take input 
+# parameters from user 
+# 10. Write a program to find area of triangle. (Area=(l*b)/2). Take input 
+# parameters from user 
+# 11. WAP to convert the given temperature from Fahrenheit to Celsius using the 
+# formula C = (F – 32) / 1.8 
+# 12. WAP to convert temperature from Celsius to Fahrenheit where temperature 
+# in Celsius is entered by user.  
+# (F= C*(9/5) + 32) 
+# 13. Take a number as user input and convert it into Binary, Octal and 
+# Hexadecimal numbers 
+# 14. Take binary, octal and hexadecimal numbers as an input and convert them to 
+# Decimal number 
+# 15. Take 3 different inputs from user and display their data type. 
+# 16. Perform binary “AND” and “OR” operation for given 2 integer numbers 
+# from user input 
+# 17. Write a program to calculate area of circle. (pi*r*r) 
+# 18. Write a program in C to calculate simple interest using formula SI = 
+# (P*R*N) / 100. Take all parameters as input from user 
+# 19. Without applying condition statement display output as “true” if a number is 
+# an even number and “false” if the number is an odd number 
+# 20. Write a program to swap two numbers using a third variable and also 
+# without using the third variable. 
---
+++ py_day2/2,3.py
@@ -0,0 +1,8 @@
+
+# 2. Write a python program to get user input using input( ) function 
+# 3. WAP to take only numerical input 
+
+#-> ;lets combine 2-3
+
+two_three = int(input("enter only numerical data : "))
+print(two_three)
